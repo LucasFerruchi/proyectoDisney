1c5f0d1d7533f4b0462f1298db561cdffad7cd39
JuaniBurgos <juanburgos86b@gmail.com>
2021-03-23 20:50:17 -0300

    Se maqueto el admin.html y se creo la clasePeliculas, jutno al admin.js

---
+++ js/peliculasClass.js
@@ -0,0 +1,11 @@
+export class Pelis {
+    constructor(codigo, nombre, categoria, descripcion, imagen, publicada, destacada) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.publicada = publicada;
+        this.destacada = destacada;
+    }
+}
