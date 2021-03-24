c69d154072031cfefee05f9d9eddb294490f40da
JuaniBurgos <juanburgos86b@gmail.com>
2021-03-24 20:27:05 -0300

    Se agrago funcionalidad para agregar una pelicula al localStorage y dibujar todos los datos en la tabla

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,78 @@
 import { Pelis } from './peliculasClass.js'
-console.log(Pelis);
-let nuevaPelicula = new Pelis(1, "La Ley", "123", "456", "png", true, false);
-console.log(nuevaPelicula);
+let listaPeliculas = [];
+const modalPeli = new bootstrap.Modal(document.getElementById('modalPeliculas'))
+
+let btnAgregar = document.getElementById("btnAgregar");
+btnAgregar.addEventListener("click", function() {
+    modalPeli.show();
+})
+leerPeliculas();
+
+// Funcion para agregar una película 
+window.agregarPelicula = function(event) {
+    event.preventDefault();
+    console.log("desde agregarPeliculas")
+        //validar general if (validarGeneral)
+    let codigo = document.getElementById("codigo").value;
+    let nombrePeli = document.getElementById("nombPeli").value;
+    let categoria = document.getElementById("categoria").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let imagen = document.getElementById("imagen").value;
+    let publicada = document.getElementById("publicada").value;
+    let destacada = document.getElementById("destacada").value;
+
+    let nuevaPelicula = new Pelis(codigo, nombrePeli, categoria, descripcion, imagen, publicada, destacada);
+    //console.log(nuevaPelicula.publicada);
+    //console.log(nuevaPelicula.destacada);
+    listaPeliculas.push(nuevaPelicula);
+    //console.log(listaPeliculas);
+
+    localStorage.setItem('listaPeliculasKey', JSON.stringify(listaPeliculas));
+    limpiarFormulario();
+    // Ventana de SweetAlert2
+    Swal.fire(
+        'Pelicula Agregada',
+        'La película se agrego con éxito!',
+        'success'
+    )
+    leerPeliculas();
+    modalPeli.hide();
+}
+
+function limpiarFormulario() {
+    document.getElementById("formPeli").reset();
+}
+
+//Agrego la lectura del localStorage
+function leerPeliculas() {
+    let _listaPeliculas;
+    if (localStorage.length > 0) {
+        _listaPeliculas = JSON.parse(localStorage.getItem('listaPeliculasKey'));
+        //console.log(_listaPeliculas);
+        if (listaPeliculas.length === 0) {
+            listaPeliculas = _listaPeliculas;
+        }
+    }
+    dibujarTabla(_listaPeliculas);
+}
+//Dibujo los datos del localStorage en la tabla
+function dibujarTabla(_listaPeliculas) {
+    let tabla = document.getElementById("tablaPeliculas")
+    tabla.innerHTML = "";
+    let filas;
+    for (let i in _listaPeliculas) {
+        filas = `<tr>
+        <td>${_listaPeliculas[i].codigo}</td>
+        <td>${_listaPeliculas[i].nombre}</td>
+        <td>${_listaPeliculas[i].categoria}</td>
+        <td>${_listaPeliculas[i].publicada}</td>
+        <td>${_listaPeliculas[i].destacada}</td>
+        <td>
+            <button class="btn btn-primary"><i class="far fa-star"></i></button>
+            <button class="btn btn-warning"><i class="fas fa-marker"></i></button>
+            <button class="btn btn-danger"><i class="fas fa-trash-restore"></i></button>
+        </td>
+        </tr>`
+        tabla.innerHTML += filas;
+    }
+}
